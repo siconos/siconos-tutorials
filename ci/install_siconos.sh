ed6d6a269626f34ea66c58dc0a6475b1be7d1ed6
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2018-10-02 16:37:20 +0200

    Update install_siconos.sh

--- ci/install_siconos.sh
+++ ci/install_siconos.sh
@@ -5,5 +5,5 @@ git clone https://github.com/siconos/siconos.git
 mkdir build
 cd build
 cmake ../siconos -DUSER_OPTIONS_FILE=$PWD/../ci/siconos_conf.cmake -DCMAKE_INSTALL_PREFIX=../install-siconos
-make -j 4
+make -j 8
 make install
