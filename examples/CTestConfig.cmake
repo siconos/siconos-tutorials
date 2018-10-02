49d381534ef318488c8c4caa00197e2c698f44ef
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2018-10-02 15:36:11 +0200

    cdash conf

---
+++ examples/CTestConfig.cmake
@@ -0,0 +1,19 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+
+set(CTEST_PROJECT_NAME "Siconos examples")
+set(CTEST_NIGHTLY_START_TIME "20:00:00 CET")
+
+# https is needed on cdash server side
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "cdash-tripop.inrialpes.fr")
+set(CTEST_DROP_LOCATION "/submit.php?project=Siconos")
+set(CTEST_DROP_SITE_CDASH TRUE)
+if(BUILD_NAME)
+  set(CTEST_BUILD_NAME Siconos examples)
+endif()
