4a35987b0eb403196c733a719eb47bf960fe7a54
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2018-10-02 16:27:10 +0200

    setup ci. Draft

--- ci/build_examples.sh
+++ ci/build_examples.sh
@@ -4,4 +4,5 @@ mkdir build-examples
 cd build-examples
 export siconos_DIR=../install-siconos/share/siconos/cmake
 cmake ../examples
-make -j 4
+make -j 8
+make test -j 8
--- ci/run_examples.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-cd build-examples
-make test -j 4
