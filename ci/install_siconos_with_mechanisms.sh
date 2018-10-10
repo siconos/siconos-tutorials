28b763abac73d658d852d492f7178091fabafea6
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2018-10-10 16:19:43 +0200

    ci, add jobs

--- ci/install_siconos_with_mechanisms.sh
+++ ci/install_siconos_with_mechanisms.sh
@@ -19,7 +19,7 @@ git clone https://github.com/tpaviot/oce.git
 git clone https://github.com/tpaviot/pythonocc-core.git
 mkdir build
 cd build
-mkdir oce-last pythonocc
+mkdir oce-last pythonocc siconos
 cd oce-last
 cmake ../../oce -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release
 make -j $nbprocs
@@ -35,10 +35,8 @@ git clone https://github.com/siconos/siconos.git
 cd siconos
 git rev-parse --short HEAD > ${ref_path}/siconos-commit-number.txt
 #
-cd ..
-mkdir build
-cd build
-ctest -S ${ref_path}/ci/ctest_driver.cmake -V -DJOB_NAME=siconos_install -Dmodel=Continuous -DWITH_TESTS=ON -DSICONOS_INSTALL_DIR=${ref_path}/install-siconos -DOSNAME=$1 -DUSER_FILE=siconos_with_mechanisms.cmake -DCTEST_BUILD_NAME="Siconos with mechanisms"
+cd ${ref_path}/build/siconos
+ctest -S ${ref_path}/ci/ctest_driver.cmake -V -DJOB_NAME=siconos_install -Dmodel=Continuous -DSICONOS_INSTALL_DIR=${ref_path}/install-siconos -DOSNAME=$1 -DUSER_FILE=siconos_with_mechanisms.cmake -DCTEST_BUILD_NAME="Siconos with mechanisms"
 
 #make -j ${nbprocs}
 make install
