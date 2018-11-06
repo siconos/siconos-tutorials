e02c394f10b19743a46916799fbd736f78923ba6
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2018-11-06 17:16:54 +0100

    [CI] fix oce install process and try to reuse install between jobs

--- ci/install_oce.sh
+++ ci/install_oce.sh
@@ -15,6 +15,9 @@ cd build
 export build_path=$PWD
 
 #  -- OCE --
+# If 'clone_oce' is set as input arg of the present script,
+# download and install the last version of oce.
+mkdir -p $CI_PROJECT_DIR/install/
 
 if [ "$#" -eq 1 ] && [ "$1" = "clone_oce" ]; then 
     # If "clone_oce" as third arg of this script ...
@@ -22,23 +25,34 @@ if [ "$#" -eq 1 ] && [ "$1" = "clone_oce" ]; then
     git clone https://github.com/tpaviot/oce.git
     mkdir oce-last
     cd oce-last
-    cmake ../oce -DOCE_INSTALL_PREFIX=$HOME/install_oce  -Wno-deprecated -DCMAKE_BUILD_TYPE=Release
+    # Warning : install in 'user' path, that will be transfered between jobs (artifacts)
+    cmake ../oce -DOCE_INSTALL_PREFIX=$CI_PROJECT_DIR/install/oce  -Wno-deprecated -DCMAKE_BUILD_TYPE=Release
     make -j $nbprocs > /dev/null
     make install > /dev/null
+    # Save path to OCEConfig.cmake, required to configure pythonocc
+    export OCE_INSTALL=`grep OCEConfig.cmake install_manifest.txt| sed 's/OCEConfig.cmake//g'`
+    oce_option="-DOCE_DIR=$OCE_INSTALL"
     cd $build_path
-    export OCE_INSTALL=$HOME/install_oce
 else
     echo "Use installed version of oce (package?)"
+    oce_option=""
 fi
 
 # -- Python occ --
+# Clone last pythonocc version.
+# We assume it is complient with the installed oce version.
+# Maybe we should clone specific tags for oce and pythonocc? 
 git clone https://github.com/tpaviot/pythonocc-core.git
 mkdir pythonocc
 cd pythonocc
 # Requires (in calling script):
-# export installpath=`python3 -c "import site;print(site.USER_SITE)"`
-cmake ../pythonocc-core -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DOCE_DIR=$OCE_INSTALL -DPYTHONOCC_INSTALL_DIRECTORY=$installpath
+# installpath=`python3 -c "import site;print(site.USER_SITE)"`# Unfortunately, this cannot work, artifacts must be
+# in CI_PROJECT_DIR ...
+export pyocc_installpath=$CI_PROJECT_DIR/install/site-packages
+# Mind the OCC at the end of the install path!
+cmake ../pythonocc-core -DCMAKE_BUILD_TYPE=Release -Wno-deprecated $oce_option -DPYTHONOCC_INSTALL_DIRECTORY=$pyocc_installpath/OCC
 make install -j $nbprocs > /dev/null
 cd $build__path
 # test ...
+export PYTHONPATH=$installpath
 python3 -c 'import OCC; print(OCC.__file__)'
