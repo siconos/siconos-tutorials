4996a9b562017b2e67fa6e4ecdefb7e14fd842fa
Vincent Acary <vincent.acary@inria.fr>
2019-06-04 16:53:19 +0200

    [examples] use convex hull rather than box to be able to take into account margin

--- examples/mechanics/RollingFriction/spheres_pile.py
+++ examples/mechanics/RollingFriction/spheres_pile.py
@@ -25,25 +25,51 @@ with MechanicsHdf5Runner() as io:
     # Definition of a sphere
     io.add_primitive_shape('Sphere', 'Sphere',
                            (diameter/2.0,),
-                           insideMargin=0.0, #diameter*0.1,
-                           outsideMargin=0.0)
+                           insideMargin=diameter*0.0,
+                           outsideMargin=diameter*0.001)
 
     # Definition of the ground shape
-    io.add_primitive_shape('Ground', 'Box', (50*diameter,50*diameter , diameter))
+
+    box_x_scale=30*diameter
+    box_y_scale=30*diameter
+    box_z_scale=2*diameter
+
+
+    # We use a convex hull rather than a box primitive. With the box primitive, the outside margin
+    # are not taken into account. With multipoints_iterations=False, i.e, only one contact point
+    # the spheres are slowly penetrating the ground. This is a well-known issue with bullet collision
+    # engine between Sphere and Box
+
+    # io.add_primitive_shape('Ground', 'Box', (50*diameter,50*diameter , diameter))
+
+
+    vertices = [(0*box_x_scale,0*box_y_scale,0*box_z_scale),
+                (0*box_x_scale,1*box_y_scale,0*box_z_scale),
+                (1*box_x_scale,0*box_y_scale,0*box_z_scale),
+                (1*box_x_scale,1*box_y_scale,0*box_z_scale),
+                (0*box_x_scale,0*box_y_scale,1*box_z_scale),
+                (0*box_x_scale,1*box_y_scale,1*box_z_scale),
+                (1*box_x_scale,0*box_y_scale,1*box_z_scale),
+                (1*box_x_scale,1*box_y_scale,1*box_z_scale)]
+
+    io.add_convex_shape('Ground', vertices, outsideMargin=diameter*0.001)
+
+
 
     test=True
     if test==True:
-        n_spheres = 5
+        n_spheres = 10
     else:
-        n_spheres = 1000
+        n_spheres = 250
         
     delta_tob = math.sqrt(2.0*diameter/9.81)
     print('delta_tob', delta_tob)
     T = (n_spheres*delta_tob)*1.1
+    #T = 10*5e-3
     print('T', T)
     for s in range(n_spheres):
         tob = s*delta_tob
-        trans =  [0.5*diameter*random.random(), 0.5*diameter*random.random(), 15*diameter]
+        trans =  [0.5*diameter*random.random(), 0.5*diameter*random.random(), 7*diameter]
         print(trans)
         io.add_object('sphere_'+str(s), [Contactor('Sphere')],
                       translation=trans,
@@ -55,7 +81,7 @@ with MechanicsHdf5Runner() as io:
     # not given, it is a static object only involved in contact
     # detection.
     io.add_object('ground', [Contactor('Ground')],
-                  translation=[-0, 0, 0])
+                  translation=[-box_x_scale/2.0, -box_y_scale/2.0, 0])
 
     # Definition of a non smooth law. As no group ids are specified it
     # is between contactors of group id 0.
@@ -86,6 +112,7 @@ with MechanicsHdf5Runner(mode='r+') as io:
            set_external_forces=None,
            solver=Numerics.SICONOS_FRICTION_3D_NSGS,
            itermax=1000,
-           tolerance=1e-4,
+           tolerance=1e-04,
+           violation_verbose=True,
            numerics_verbose=False,
            output_frequency=None)
