64355f5017939f419dd81872c45294e434ed6b78
Alexandre Rocca <arocca@inrialpes.fr>
2019-06-28 18:18:54 +0200

    More reference results on the switching dae example

--- .sandbox/hybrid-daes/dae-abs-and-sign/absolute-value-and-sign-lcp.cpp
+++ .sandbox/hybrid-daes/dae-abs-and-sign/absolute-value-and-sign-lcp.cpp
@@ -40,9 +40,9 @@ int main(int argc, char* argv[])
 
         double t0       = 0.0;          // initial computation time
         double T        = 0.05;         // final computation time 
-        // double T        = 5.0;            // final computation time 
+        // double T        = 10.0;            // final computation time 
 
-        double h        =  0.05;        // time step
+        double h        =  1.05;        // time step
 
         double x1_0     = -1.0;         // initial condition in state variable x1
         double x2_0     = 0.0;          // initial condition in state variable x2
@@ -236,7 +236,7 @@ int main(int argc, char* argv[])
         // --- Output files ---
         cout << "====> Output file writing ..." << endl;
         dataPlot.resize(k, outputSize);
-        ioMatrix::write("result_Start-CONTINUE_absolute-value-and-sign-lcp.dat", "ascii", dataPlot, "noDim");
+        ioMatrix::write("result_CONTINUE-big-h_absolute-value-and-sign-lcp.dat", "ascii", dataPlot, "noDim");
     }
 
     catch (SiconosException& e)    
