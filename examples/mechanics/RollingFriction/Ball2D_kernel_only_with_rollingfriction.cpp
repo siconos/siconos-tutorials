9b362e99998bc1ba747fe43a6b6dc272dc08beb8
Vincent ACARY <vincent.acary@inria.fr>
2020-03-09 15:58:55 +0100

    [examples] add simple test with 2d rolling friction

---
+++ examples/mechanics/RollingFriction/Ball2D_kernel_only_with_rollingfriction.cpp
@@ -0,0 +1,264 @@
+/* Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ *
+ * Copyright 2018 INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+/*!\file BouncingBallTS.cpp
+  \brief \ref EMBouncingBall - C++ input file, Time-Stepping version -
+  V. Acary, F. Perignon.
+
+  A Ball bouncing on the ground.
+  Direct description of the model.
+  Simulation with a Time-Stepping scheme.
+*/
+#include <boost/timer/timer.hpp>
+#include "SiconosKernel.hpp"
+
+using namespace std;
+
+class MyCollisionManager : public InteractionManager
+{
+protected:
+  /** serialization hooks
+   */
+  ACCEPT_SERIALIZATION(MyCollisionManager);
+  /** radius of the ball */
+  double _R ;
+
+public:
+  MyCollisionManager(double R) : InteractionManager() {_R=R;}
+  virtual ~MyCollisionManager() {}
+
+  /** Called by Simulation after updating positions prior to starting
+   * the Newton loop. */
+  void updateInteractions(SP::Simulation simulation) {
+    // std::cout<< "Call to updateInteractions in MyCollisionManager" << std::endl;
+    InteractionsGraph::VIterator ui, uiend;
+    SP::InteractionsGraph indexSet0 = simulation->nonSmoothDynamicalSystem()->topology()->indexSet0();
+    for(std11::tie(ui, uiend) = indexSet0->vertices(); ui != uiend; ++ui)
+    {
+      SP::Interaction inter(indexSet0->bundle(*ui));
+      //inter->display();
+      if (inter->number()==0)
+      {
+        SP::Lagrangian2d2DR r = std11::static_pointer_cast<Lagrangian2d2DR> (inter->relation());
+        SP::LagrangianLinearTIDS ds1(std11::dynamic_pointer_cast<LagrangianLinearTIDS>(
+                                       indexSet0->properties(*ui).source));
+        SP::SiconosVector rpc = r->relPc1();
+        SP::SiconosVector nnc = r->relNc();
+        SP::SiconosVector q = ds1->q();
+        double angle= (*q)(2);
+        // std::cout << "angle = " << angle << std::endl;
+        (*rpc)(0) = -_R* cos(angle);
+        (*rpc)(1) = _R* sin(angle);
+        (*nnc)(0) = 1.0;
+        (*nnc)(1) = 0.0;
+      }
+    }
+  }
+};
+DEFINE_SPTR(MyCollisionManager);
+
+
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+
+    // ================= Creation of the model =======================
+
+    // User-defined main parameters
+    unsigned int nDof = 3;           // degrees of freedom for the ball
+    double t0 = 0;                   // initial computation time
+    double T = 10;                  // final computation time
+    double h = 0.005;                // time step
+    double position_init = 1.0;      // initial position for lowest bead.
+    double velocity_init = 0.0;      // initial velocity for lowest bead.
+    double rotation_init = 2.0;      // initial velocity for lowest bead.
+    double theta = 0.5;              // theta for MoreauJeanOSI integrator
+    double R = 0.5; // Ball radius
+    double m = 1; // Ball mass
+    double g = 9.81; // Gravity
+    // -------------------------
+    // --- Dynamical systems ---
+    // -------------------------
+
+    cout << "====> Model loading ..." <<  endl;
+
+    SP::SiconosMatrix Mass(new SimpleMatrix(nDof, nDof));
+    (*Mass)(0, 0) = m;
+    (*Mass)(1, 1) = m;
+    (*Mass)(2, 2) = 2. / 5 * m * R * R;
+
+    // -- Initial positions and velocities --
+    SP::SiconosVector q0(new SiconosVector(nDof));
+    SP::SiconosVector v0(new SiconosVector(nDof));
+    (*q0)(0) = position_init;
+    (*q0)(1) = 0.0;
+    (*v0)(0) = velocity_init;
+    (*v0)(2) = rotation_init;
+
+    // -- The dynamical system --
+    SP::LagrangianLinearTIDS ball(new LagrangianLinearTIDS(q0, v0, Mass));
+
+    // -- Set external forces (weight) --
+    SP::SiconosVector weight(new SiconosVector(nDof));
+    (*weight)(0) = -m * g;
+    ball->setFExtPtr(weight);
+
+    // --------------------
+    // --- Interactions ---
+    // --------------------
+
+    // -- nslaw --
+    double e = 0.9;
+
+    // // Interaction ball-floor
+    // //
+    // SP::SimpleMatrix H(new SimpleMatrix(1, nDof));
+    // (*H)(0, 0) = 1.0;
+    // SP::Relation relation(new LagrangianLinearTIR(H));
+
+
+    SP::NonSmoothLaw nslaw(new NewtonImpactRollingFrictionNSL(e,0.0,0.1,0.,3));
+
+    SP::Lagrangian2d3DR relation(new Lagrangian2d3DR());
+
+    SP::Interaction inter(new Interaction(nslaw, relation));
+
+    // -------------
+    // --- Model ---
+    // -------------
+    SP::NonSmoothDynamicalSystem bouncingBall(new NonSmoothDynamicalSystem(t0, T));
+
+    // add the dynamical system in the non smooth dynamical system
+    bouncingBall->insertDynamicalSystem(ball);
+
+    // link the interaction and the dynamical system
+    bouncingBall->link(inter, ball);
+
+    // ------------------
+    // --- Simulation ---
+    // ------------------
+
+    // -- (1) OneStepIntegrators --
+    SP::MoreauJeanOSI OSI(new MoreauJeanOSI(theta));
+
+
+    // -- (2) Time discretisation --
+    SP::TimeDiscretisation t(new TimeDiscretisation(t0, h));
+
+    // -- (3) one step non smooth problem
+    SP::OneStepNSProblem osnspb(new RollingFrictionContact(3));
+    //SP::OneStepNSProblem osnspb(new LCP());
+
+    // -- (4) Simulation setup with (1) (2) (3)
+    SP::TimeStepping s(new TimeStepping(bouncingBall, t, OSI, osnspb));
+
+    SP::MyCollisionManager collision_manager(new MyCollisionManager(R));
+    s->insertInteractionManager(collision_manager);
+
+
+
+    // =========================== End of model definition ===========================
+
+    // ================================= Computation =================================
+
+
+    int N = ceil((T - t0) / h)+1; // Number of time steps
+
+    // --- Get the values to be plotted ---
+    // -> saved in a matrix dataPlot
+    unsigned int outputSize = 10;
+    SimpleMatrix dataPlot(N + 1, outputSize);
+
+    SP::SiconosVector q = ball->q();
+    SP::SiconosVector v = ball->velocity();
+    SP::SiconosVector p = ball->p(1);
+    SP::SiconosVector lambda = inter->lambda(1);
+
+
+    dataPlot(0, 0) =  s->nextTime();
+    dataPlot(0, 1) = (*q)(0);
+    dataPlot(0, 2) = (*v)(0);
+    dataPlot(0, 3) = (*v)(2);
+    dataPlot(0, 4) = (*p)(0);
+    dataPlot(0, 5) = (*lambda)(0);
+    dataPlot(0, 6) = (*q)(1);
+    dataPlot(0, 7) = (*q)(2);
+    dataPlot(0, 8) = (*v)(1);
+
+    // --- Time loop ---
+    cout << "====> Start computation ... " << endl;
+    // ==== Simulation loop - Writing without explicit event handling =====
+    int k = 1;
+    boost::timer::auto_cpu_timer time;
+    SP::SiconosVector rpc = relation->relPc1();
+    SP::SiconosVector nnc = relation->relNc();
+
+    while (s->hasNextEvent())
+    {
+      osnspb->setNumericsVerboseMode(1);
+
+      s->computeOneStep();
+      osnspb->display();
+
+      // --- Get values to be plotted ---
+      dataPlot(k, 0) =  s->nextTime();
+      dataPlot(k, 1) = (*q)(0);
+      dataPlot(k, 2) = (*v)(0);
+      dataPlot(k, 3) = (*v)(2);
+      dataPlot(k, 4) = (*p)(0);
+      dataPlot(k, 5) = (*lambda)(0);
+      dataPlot(k, 6) = (*q)(1);
+      dataPlot(k, 7) = (*q)(2);
+      dataPlot(k, 8) = (*v)(1);
+       s->nextStep();
+       // getchar();
+      k++;
+
+    }
+    cout  << "End of computation - Number of iterations done: " << k - 1 << endl;
+    cout << "Computation Time :"  << endl;
+    time.report();
+    // --- Output files ---
+    cout << "====> Output file writing ..." << endl;
+    dataPlot.resize(k, outputSize);
+    ioMatrix::write("Ball_2d_with_kernel_only_with_rollingfriction.dat", "ascii", dataPlot, "noDim");
+    double error=0.0, eps=1e-12;
+    if ((error=ioMatrix::compareRefFile(dataPlot, "Ball_2d_with_kernel_only_with_rollingfriction.ref", eps)) >= 0.0
+        && error > eps)
+      return 1;
+
+  }
+
+  catch (SiconosException& e)
+  {
+    cerr << e.report() << endl;
+    return 1;
+
+  }
+  catch (...)
+  {
+    cerr << "Exception caught in BouncingBallTS.cpp" << endl;
+    return 1;
+
+  }
+
+
+
+}
---
+++ examples/mechanics/RollingFriction/disk_on_box.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+#
+# Example of one object under gravity with one contactor and a ground
+# using the Siconos proposed mechanics API
+#
+
+from siconos.mechanics.collision.tools import Contactor
+from siconos.io.mechanics_run import MechanicsHdf5Runner
+from siconos.mechanics.collision.bullet import SiconosBulletOptions
+import siconos.numerics as sn
+import siconos.kernel as sk
+# Creation of the hdf5 file for input/output
+with MechanicsHdf5Runner() as io:
+
+    # Definition of a sphere
+    io.add_primitive_shape('Disk', 'Disk', (2,),
+                           insideMargin=0.0, outsideMargin=0.0)
+    
+    # Definition of the ground shape
+    io.add_primitive_shape('Ground', 'Box2d', (10, 1),
+                           insideMargin=0.0, outsideMargin=0.0)
+
+    # Definition of a non smooth law. As no group ids are specified it
+    # is between contactors of group id 0.
+    io.add_Newton_impact_rolling_friction_nsl('contact', mu=0.1, mu_r=0.1, e=0.5)
+    #io.add_Newton_impact_friction_nsl('contact', mu=0.1, e=0.5)
+
+    # The sphere object made with an unique Contactor : the sphere shape.
+    # As a mass is given, it is a dynamic system involved in contact
+    # detection and in the simulation.  With no group id specified the
+    # Contactor belongs to group 0
+    io.add_object('disk', [Contactor('Disk')],
+                  translation=[0, 5.],
+                  velocity=[0, 0, 2.5],
+                  mass=1., inertia =2.0)
+    
+    # the ground object made with the ground shape. As the mass is
+    # not given, it is a static object only involved in contact
+    # detection.
+    io.add_object('ground', [Contactor('Ground')],
+                  translation=[0, -.5])
+
+# Run the simulation from the inputs previously defined and add
+# results to the hdf5 file. The visualisation of the output may be done
+# with the vview command.
+
+bullet_options = SiconosBulletOptions()
+bullet_options.worldScale = 1.0
+bullet_options.contactBreakingThreshold = 0.04
+bullet_options.dimension = 1
+
+
+options = sk.solver_options_create(sn.SICONOS_ROLLING_FRICTION_2D_NSGS)
+#options = sk.solver_options_create(sn.SICONOS_FRICTION_2D_NSGS)
+options.iparam[sn.SICONOS_IPARAM_MAX_ITER] = 100000
+options.dparam[sn.SICONOS_DPARAM_TOL] = 1e-8
+
+
+with MechanicsHdf5Runner(mode='r+') as io:
+
+    # By default earth gravity is applied and the units are those
+    # of the International System of Units.
+    io.run(verbose=True,
+        with_timer=False,
+           bullet_options=bullet_options,
+           face_class=None,
+           edge_class=None,
+           t0=0,
+           T=6,
+           h=0.001,
+           multipoints_iterations=True,
+           theta=0.50001,
+           Newton_max_iter=1,
+           set_external_forces=None,
+           solver_options=options,
+           numerics_verbose=True,
+           output_frequency=None)
