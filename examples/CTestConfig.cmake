c0cf0ff73b5e1bf1f9c6a4c7770ffeeb2d7f01b6
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2020-03-13 11:26:44 +0100

    [docker-build] update cdash site, fix (?) siconos options to include bullet examples

--- examples/CTestConfig.cmake
+++ examples/CTestConfig.cmake
@@ -7,11 +7,10 @@
 ##   INCLUDE(CTest)
 
 set(CTEST_PROJECT_NAME "Siconos examples")
-set(CTEST_NIGHTLY_START_TIME "20:00:00 CET")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 CET")
 
 # https is needed on cdash server side
-set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org/")
-# set(CTEST_DROP_SITE "cdash-tripop.inrialpes.fr")
-set(CTEST_DROP_LOCATION "/submit.php?project=Siconos")
+set(CTEST_DROP_LOCATION "/submit.php?project=Siconos+Examples")
 set(CTEST_DROP_SITE_CDASH TRUE)
