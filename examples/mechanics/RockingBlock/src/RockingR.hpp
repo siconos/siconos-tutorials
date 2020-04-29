267be0f5cc4fb48b907063447e6ae2ec9399d11c
Vincent ACARY <vincent.acary@inria.fr>
2020-04-29 15:05:29 +0200

    [docker-build] [examples] correct wrong signature in derivated class

--- examples/mechanics/RockingBlock/src/RockingR.hpp
+++ examples/mechanics/RockingBlock/src/RockingR.hpp
@@ -28,14 +28,6 @@ public:
   void computeDotJachq(const BlockVector& q, BlockVector& z, const BlockVector& qdot)
 
   {
-    // TMP : this stage won't be necessary when the LagrangianScleronomousR
-    // class will be updated
-    if(!_dotjachq)
-    {
-      unsigned int sizeY = 1;
-      unsigned int sizeDS = 3;
-      _dotjachq.reset(new SimpleMatrix(sizeY, sizeDS));
-    }
     double q2 = q.getValue(2);
     double qdot2 = qdot.getValue(2);
     _dotjachq->setValue(0, 0, 0.0);
@@ -59,24 +51,16 @@ public:
     y.setValue(0, q1 + 0.5 * LengthBlock * sin(q2) - 0.5 * HeightBlock * cos(q2));
   }
 
-  void computeJachq(SiconosVector& q, BlockVector& z)
+  void computeJachq(const BlockVector& q, BlockVector& z)
   {
     double q2 = q.getValue(2);
     _jachq->setValue(0, 0, 0.0);
     _jachq->setValue(0, 1, 1.0);
     _jachq->setValue(0, 2, 0.5 * LengthBlock * cos(q2) + 0.5 * HeightBlock * sin(q2));
   }
-
+  
   void computeDotJachq(const BlockVector& q, BlockVector& z, const BlockVector& qdot)
   {
-    // TMP : this stage won't be necessary when the LagrangianScleronomousR
-    // class will be updated
-    if(!_dotjachq)
-    {
-      unsigned int sizeY = 1;
-      unsigned int sizeDS = 3;
-      _dotjachq.reset(new SimpleMatrix(sizeY, sizeDS));
-    }
     double q2 = q.getValue(2);
     double qdot2 = qdot.getValue(2);
     _dotjachq->setValue(0, 0, 0.0);
