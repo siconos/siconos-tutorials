302ccfa3f6983fc6ffb1ebd16b7cd85d6d2098ad
Vincent ACARY <vincent.acary@inria.fr>
2024-02-22 08:37:06 +0100

    [examples/mechanics/GeometricPrimitives] box_stacks with run_options

--- examples/mechanics/GeometricPrimitives/box_stacks.py
+++ examples/mechanics/GeometricPrimitives/box_stacks.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 from siconos.mechanics.collision.tools import Contactor
-from siconos.io.mechanics_run import MechanicsHdf5Runner
+from siconos.io.mechanics_run import MechanicsHdf5Runner, MechanicsHdf5Runner_run_options
+from siconos.mechanics.collision.bullet import SiconosBulletOptions
 
 import siconos.numerics as sn
 import siconos.kernel as sk
@@ -57,25 +58,51 @@ with MechanicsHdf5Runner() as io:
     # is between contactors of group id 0.
     io.add_Newton_impact_friction_nsl('contact', mu=0.3)
 
+
+bullet_options = SiconosBulletOptions()
+bullet_options.worldScale = 1.
+bullet_options.contactBreakingThreshold = 0.4
+bullet_options.perturbationIterations = 3.
+bullet_options.minimumPointsPerturbationThreshold = 3.
+
 options = sk.solver_options_create(sn.SICONOS_FRICTION_3D_NSGS)
-options.iparam[sn.SICONOS_IPARAM_MAX_ITER] = 100000
-options.dparam[sn.SICONOS_DPARAM_TOL] = 1e-8
+options.iparam[sn.SICONOS_IPARAM_MAX_ITER] = 1000
+options.dparam[sn.SICONOS_DPARAM_TOL] = 1e-4
+options.iparam[sn.SICONOS_FRICTION_3D_NSGS_FREEZING_CONTACT] = 10
+
+run_options=MechanicsHdf5Runner_run_options()
+run_options['t0']=0
+run_options['T']=1.0
+run_options['h']=0.01
+#run_options['theta']=1.
+
+run_options['Newton_max_iter'] =1
+run_options['Newton_tolerance'] =1e-6
+
+run_options['bullet_options']=bullet_options
+run_options['solver_options']=options
 
 
+#run_options['skip_last_update_output']=True
+#run_options['skip_reset_lambdas']=True
+run_options['osns_assembly_type']= sk.REDUCED_DIRECT
+
+run_options['verbose']=True
+run_options['with_timer']=True
+#run_options['explode_Newton_solve']=True
+#run_options['explode_computeOneStep']=True
+
+#run_options['violation_verbose'] = True
+run_options['output_frequency']=1
 
 test=True
 if test:
-    T=1.0
+    run_options['T']=1.0
     options.iparam[sn.SICONOS_IPARAM_MAX_ITER] = 1000
-    options.dparam[sn.SICONOS_DPARAM_TOL] = 1e-3
+    options.dparam[sn.SICONOS_DPARAM_TOL] = 1e-06
 else:
-    T=10.0
+    run_options['T']=10.0
+
 # Load and run the simulation
 with MechanicsHdf5Runner(mode='r+') as io:
-    io.run(t0=0,
-           T=T,
-           h=0.01,
-           theta=0.5,
-           Newton_max_iter=1,
-           solver_options=options,
-           output_frequency=1)
+    io.run(run_options)
